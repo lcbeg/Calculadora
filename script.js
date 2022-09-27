6a9217b0f5beb99deb855c4e3ef666e927b73733
Clari Betancourt <lcbeg@ier.unam.mx>
2022-09-27 18:19:36 -0500

    feat: faltaba la división, se agregó

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calcula(op) {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
     let operacion;
-    
+
     switch (op) {
         case "+":
             operacion = (a, b) => a + b;
@@ -14,6 +14,9 @@ function calcula(op) {
             break;
         case "*":
             operacion = (a, b) => a * b;
+            break;
+        case "/":
+            operacion = (a, b) => a / b;
             break;
     }
     document.getElementById("resultado").innerHTML = operacion(num1, num2);
